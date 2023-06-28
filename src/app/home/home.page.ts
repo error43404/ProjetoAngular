9156b56d3e66f31be409cfcdac61757a4f11e002
error43404 <135171205+error43404@users.noreply.github.com>
2023-06-28 00:50:19 +0000

    yes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,5 +10,63 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule],
 })
 export class HomePage {
-  constructor() {}
+  operations = ['+', '-', 'x', '÷'];
+
+  operationsValue = '';
+
+  result = '';
+
+  constructor() {
+  }
+
+  buttonNumericClick(number) {
+      this.operationsValue += '' + number;
+      this.calc();
+  }
+
+  buttonCalcClick(operationCalc) {
+      if (this.isLastOperationCalc()) {
+          this.operationsValue = this.operationsValue.substr(0, this.operationsValue.length - 1);
+      }
+      this.operationsValue += operationCalc;
+  }
+
+  buttonClearClick() {
+      this.operationsValue = this.operationsValue.substr(0, this.operationsValue.length - 1);
+      this.calc();
+  }
+
+  isLastOperationCalc() {
+      if (this.operationsValue.length === 0) {
+          return false;
+      }
+      const lastElemet = this.operationsValue.substr(this.operationsValue.length - 1);
+      return this.operations.indexOf(lastElemet) >= 0;
+  }
+
+  hasOperationCalc() {
+      if (this.operationsValue.length === 0) {
+          return false;
+      }
+      return this.operations.some(operation => this.operationsValue.indexOf(operation) >= 0);
+  }
+
+  calc() {
+      if (this.isLastOperationCalc() || !this.hasOperationCalc()) {
+          return;
+      }
+
+      let expression = this.operationsValue;
+      expression = this.replaceAll(expression, 'x', '*');
+      expression = this.replaceAll(expression, '÷', '/');
+
+      this.result = evaluate(expression);
+  }
+
+      replaceAll(target, search, replacement) {
+      return target.replace(new RegExp(search, 'g'), replacement);
+  }
+
+
+
 }
